62dc63cced0ebccb741d2e0de566f5872f1bf584
EvilxxxNode <emondeveloper@gmail.com>
2020-06-29 22:51:14 +0600

    Added comments & removed custom swap function

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,54 +2,11 @@
 #include <cstdlib>
 using namespace std;
 
-void swap(int *x, int *y)
-{
-	int temp;
-	temp = *x;
-	*x = *y ;
-	*y = temp;
-}
-void BubbleSort(int arr[], int n, int *u)
-{
-	int i,j;
-	bool swapped;
-	
-	for (i = 0; i < n-1; i++)
-	{
-		swapped = false;
-		for (j = 0; j < n-i-1; j++)
-		{
-			
-			if (arr[j] > arr[j+1])
-			{
-				swap(&arr[j],&arr[j+1]);
-				swapped = true;
-				(*u)++ ;
-			}
-		}
-		if (swapped == false)
-		{ 			// if inner loop no need to iterate then swapped gonna turn to
-			break;							// be false that means all elements are sorted!
-		}
-	}
-}
-void printArray(int arr[], int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << " " ;
-	}
-	cout << endl ;
-}
-void random_array(int arr[],int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = 1 + (rand() % n) ;
-		cout << arr[i] << " " ;
-	}
-	cout << endl ;
-}
+// function prototypes
+void random_array(int arr[],int n);
+void BubbleSort(int arr[], int n);
+void printArray(int arr[], int n);
+
 int main()
 {
 	int n;
@@ -61,8 +18,55 @@ int main()
 	
 	BubbleSort(arr,n,&count);
 	printArray(arr,n);
-	cout << "Needs to swap: " << count << " times" ;
-	
 	cout << endl;
 	return 0;
+}
+
+// Bubble Sort implementation
+void BubbleSort(int arr[], int n)
+{
+	int i,j;
+	bool swapped;
+	
+	for (i = 0; i < n - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < n - i - 1; j++)
+		{
+			
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j],arr[j + 1]);
+				swapped = true;
+			}
+		}
+		
+		/*if inner loop no need to iterate then swapped gonna turn to
+		be false that means all elements are sorted! */
+		if (swapped == false)
+		{ 			
+			break;							// 
+		}
+	}
+}
+
+// print the sorted array
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " " ;
+	}
+	cout << endl ;
+}
+
+// generates a random array
+void random_array(int arr[],int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = 1 + (rand() % n) ;
+		cout << arr[i] << " " ;
+	}
+	cout << endl ;
 }
