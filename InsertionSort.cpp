bdca613b14fa43a9b3fb05e6c4fb6b4a20b813c2
EvilxxxNode <emondeveloper@gmail.com>
2020-06-29 23:20:05 +0600

    Added Comments & Some Indentations

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// function prototype
 void InsertionSort(int arr[], int n);
 
 int main()
 {
     int arr[] = {5,2,42,6,1,3,2};
+	
+	// size of the array
     int n = sizeof(arr) / sizeof(arr[0]) ;
-
+	
+	// print the array elements before sorting
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << ' ' ;
@@ -15,7 +19,8 @@ int main()
     cout << endl ;
 
     InsertionSort(arr, n);
-
+	
+	// print elements of array after sorting
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << ' ' ;
@@ -24,6 +29,7 @@ int main()
     return 0;
 }
 
+// Insertion Sort implementation
 void InsertionSort(int arr[], int n)
 {
     int j = 0 ;
